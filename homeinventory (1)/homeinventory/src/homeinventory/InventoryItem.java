17f9ade16cbcaf7d41d416e52de7840c7dfe3680
Faisalfida1 <sardarfasoo01@gmail.com>
2024-05-22 22:38:58 +0500

    Add files via upload

---
+++ homeinventory (1)/homeinventory/src/homeinventory/InventoryItem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homeinventory;
+
+/**
+ *
+ * @author faizan
+ */
+class InventoryItem {
+    String description;
+    String location;
+    String serialNumber;
+    boolean marked;
+    String purchasePrice;
+    String purchaseDate;
+    String purchaseLocation;
+    String note;
+    String photoFile;
+    
+}
